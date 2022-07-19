a398492660e371d4cc60f6ff1d18d8195cea148b
YohannFrench <yc900662@student.polsl.pl>
2022-07-19 22:00:59 +0200

    main

---
+++ mail.php
@@ -0,0 +1,26 @@
+<?php
+if($_POST){
+  $firstname = $_POST['firstname'];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+  $headers .= "From: $name <$email>\r\nReply-to : $name <$email>\nX-Mailer:PHP";
+
+  $subject="Demande de contact";
+  $destinataire="yohann.coueraud@2023.icam.fr";
+  $body="$message";
+
+  if(mail($destinataire,$subject,$body,$headers)) {
+    $response['status'] = 'success';
+    $response['msg'] = 'your mail is sent';
+  } else {
+    $response['status'] = 'error';
+    $response['msg'] = 'Something went wrong';
+  }
+
+  echo json_encode($response);
+}
+?>
---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    //click on button "open_modal"
+    $('#open_modal').click(function() {
+        //Display modal by modifying display : none to block in css 
+        $('#our_modal').css(
+            {
+                'display': 'block'
+            }
+        )
+    });
+
+    //click on button "close_modal"
+    $('#close_modal').click(function() {
+        $('#our_modal').css(
+            {
+                'display': 'none'
+            }
+        )
+    });
+
+    //send mail
+    $('#send_email').click(function(e){
+		e.preventDefault(); // prevent reload pages 
+		var data = { // take every id of the form
+            firstname: $('#firstname').val(),
+			name: $('#name').val(),
+            email: $('#email').val(),
+			message: $('#message').val()
+		};
+
+		// AJAX
+		$.ajax({ //send to url -> php file
+			url: "mail.php", // url which receive data
+			type: 'POST', // Methode POST -> Send data
+			data: data, // Which data ? -> var data
+			success: function(data) {
+				$('#js_alert_success').css({'display' : 'block'});
+				setTimeout(function(){
+					$('#js_alert_success').css({'display' : 'none'}); 
+					$('#firstname').val("");
+					$('#name').val("");
+					$('#email').val("");
+					$('#message').val("")
+				}, 3000); // after 3 sec, previous inputs and alert are erased
+			},
+			error: function(data) {
+				$('#js_alert_danger').css({'display' : 'block'});
+				setTimeout(function(){
+					$('#js_alert_danger').css({'display' : 'none'});
+					$('#firstname').val("");
+					$('#name').val("");
+					$('#email').val("");
+					$('#message').val("")
+				}, 3000); // after 3 sec, previous inputs and alert are erased
+			}
+		});
+	});
+});
